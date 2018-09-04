e5d8f43036b61a31e6c2afa9dd4dd96aa436214e
Jason Lenoble <jason.lenoble@gmail.com>
2018-09-04 19:18:08 +0200

    TDD starts (and stops) db service

---
+++ gulp/docker.js
@@ -0,0 +1,42 @@
+import gulp from 'gulp';
+import {exec} from 'child_process';
+import cleanUp from 'node-cleanup';
+
+const echo = cb => (err, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+  cb(err);
+}
+
+let p;
+
+function dockerStart (cb) {
+  p = exec('docker-compose start', echo(cb));
+}
+
+function dockerStop (cb) {
+  if (p) {
+    console.log('\nStopping services...');
+    exec('docker-compose stop', echo(cb));
+  } else {
+    exec('docker-compose stop', err => {
+      process.stdout.write('');
+      cb(err);
+    });
+  }
+}
+
+cleanUp(function (exitCode, signal) {
+  if (signal) {
+    dockerStop(function done() {
+      // calling process.exit() won't inform parent process of signal
+      process.kill(process.pid, signal);
+    });
+
+    cleanUp.uninstall(); // don't call cleanup handler again
+
+    return false;
+  }
+});
+
+gulp.task('docker', gulp.series(dockerStart));
--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ try {
     require(path.join(process.cwd(), gulpDir, filename));
   });
 
-  gulp.task(autoTask, gulp.series('tdd:transpile:gulp', 'notebooks'));
+  gulp.task(autoTask, gulp.series('tdd:transpile:gulp', 'docker', 'notebooks'));
 
   // If success, start infinite dev process with autoreload
   gulp.task('default', autoreload(autoTask, gulpDir));
