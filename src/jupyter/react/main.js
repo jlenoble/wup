e3b78052c5c9b1141f78ffbd3bebdbe75c2bc7b1
Jason Lenoble <jason.lenoble@gmail.com>
2018-09-10 20:44:11 +0200

    Added CDN paths for React

--- src/jupyter/react/main.js
+++ src/jupyter/react/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
+requirejs.config({
+  paths: {
+    'react': 'https://unpkg.com/react@16/umd/react.development',
+    'react-dom': 'https://unpkg.com/react-dom@16/umd/react-dom.development',
+  }
+});
+
 define(function (require) {
   var exports = {};
 
   function load_ipython_extension() {
-    console.log('toto');
+    requirejs(['react', 'react-dom'], function (React, ReactDom) {
+      console.log(React, ReactDom);
+    });
   }
 
   exports.load_ipython_extension = load_ipython_extension;
