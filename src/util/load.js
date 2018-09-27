206e95d8575b6561797484db63cd85eafe3cfd32
Jason Lenoble <jason.lenoble@gmail.com>
2018-09-27 14:39:34 +0200

    Don't cache local files

--- src/util/load.js
+++ src/util/load.js
@@ -1,3 +1,16 @@
 export default function load (name) {
-  return require(path.join('../../../../build/src', name));
+  const srcDir = path.resolve('../../../../build/src');
+
+  // Don't cache local files
+  if (require.cache) {
+    const regex = new RegExp(`^${srcDir}/`);
+
+    Object.keys(require.cache).forEach(key => {
+      if (regex.test(key)) {
+        delete require.cache[key];
+      }
+    });
+  }
+
+  return require(path.join(srcDir, name));
 }
