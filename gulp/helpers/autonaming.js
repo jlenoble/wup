7021099b1cd70d6ae789d61663e7e9f3385afba5
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-01 15:28:02 +0200

    Twick to guard against crossing midnight

--- gulp/helpers/autonaming.js
+++ gulp/helpers/autonaming.js
@@ -2,8 +2,25 @@ import moment from 'moment';
 import path from 'path';
 import {logDir} from './dirs';
 
-export const thisMonth = () => moment().format('YYYY/MM');
+export const safeNow = () => {
+  // Make sure consecutive calls don't cross midnight
+  let now;
+
+  if (
+    moment().add(1,'days').startOf('day').isAfter(moment().add(1, 'minutes'))
+  ) {
+    now = moment();
+  } else {
+    now = moment().add(1, 'minutes')
+  }
+
+  return now;
+}
+
+export const thisMidnight = () => safeNow().add(1,'days').startOf('day');
+
+export const thisMonth = () => safeNow().format('YYYY/MM');
 export const thisMonthDir = () => path.join(logDir, thisMonth());
 
-export const today = () => moment().format('YYYY/MM/DD');
+export const today = () => safeNow().format('YYYY/MM/DD');
 export const todayNotebook = () => path.join(logDir, today() + '.ipynb');
