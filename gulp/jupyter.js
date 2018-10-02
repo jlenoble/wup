bfc454af48d785ade7b722b65cea5c40b645ae67
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-02 20:43:40 +0200

    Removed obsolete import

--- gulp/jupyter.js
+++ gulp/jupyter.js
@@ -3,7 +3,6 @@ import {spawn} from 'child_process';
 import {interceptMessage} from 'child-process-data';
 import cleanUp from './helpers/cleanup';
 import exec from './helpers/exec';
-import getMessage from './helpers/get-message';
 import {jupyterStarted} from './helpers/regex';
 
 let p;
