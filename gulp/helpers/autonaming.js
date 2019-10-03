37dc569522cf8b40fd26578470410387f8d8017f
Jason Lenoble <jason.lenoble@gmail.com>
2019-10-03 10:15:58 +0200

    Copy last notebook and change time

--- gulp/helpers/autonaming.js
+++ gulp/helpers/autonaming.js
@@ -7,20 +7,39 @@ export const safeNow = () => {
   let now;
 
   if (
-    moment().add(1,'days').startOf('day').isAfter(moment().add(1, 'minutes'))
+    moment().add(1, 'days').startOf('day').isAfter(moment().add(1, 'minutes'))
   ) {
     now = moment();
   } else {
-    now = moment().add(1, 'minutes')
+    now = moment().add(1, 'minutes');
   }
 
   return now;
-}
+};
 
-export const thisMidnight = () => safeNow().add(1,'days').startOf('day');
+export const safeBefore = n => {
+  // Make sure consecutive calls don't cross midnight
+  let before;
+
+  if (
+    moment().add(1, 'days').startOf('day').isAfter(moment().add(1, 'minutes'))
+  ) {
+    before = moment().subtract(n, 'days');
+  } else {
+    before = moment().add(1, 'minutes').subtract(n, 'days');
+  }
+
+  return before;
+};
+
+export const thisMidnight = () => safeNow().add(1, 'days').startOf('day');
 
 export const thisMonth = () => safeNow().format('YYYY/MM');
 export const thisMonthDir = () => path.join(logDir, thisMonth());
 
 export const today = () => safeNow().format('YYYY/MM/DD');
 export const todayNotebook = () => path.join(logDir, today() + '.ipynb');
+
+export const someDaysBefore = n => safeBefore(n).format('YYYY/MM/DD');
+export const someDaysBeforeNotebook = n => path.join(logDir, someDaysBefore(n)
+  + '.ipynb');
--- gulp/today.js
+++ gulp/today.js
@@ -1,20 +1,46 @@
+import fs from 'fs';
+import moment from 'moment';
 import gulp from 'gulp';
 import newer from 'gulp-newer';
 import rename from 'gulp-rename';
+import replace from 'gulp-replace';
 import gutil from 'gulp-util';
 import path from 'path';
 import {resolveMessage} from 'child-process-data';
 import exec from './helpers/exec';
 import {templateDir} from './helpers/dirs';
 import {repeatEveryDay} from './helpers/repeat';
-import {thisMonthDir, todayNotebook} from './helpers/autonaming';
+import {thisMonthDir, todayNotebook, someDaysBeforeNotebook}
+  from './helpers/autonaming';
 import {jupyterStarted, tokenAccepted} from './helpers/regex';
 
 const notebookTemplateGlob = path.join(templateDir, 'today.ipynb');
 
+function getLastNotebook () {
+  let n = 1;
+
+  for (; n < 360; n++) {
+    try {
+      fs.statSync(someDaysBeforeNotebook(n));
+      return someDaysBeforeNotebook(n);
+    } catch (e) {}
+  }
+}
+
 function createTodayNotebook () {
   const dest = thisMonthDir();
   const notebook = todayNotebook();
+  const lastNotebook = getLastNotebook();
+
+  if (lastNotebook) {
+    return gulp.src(lastNotebook)
+      .pipe(newer(notebook))
+      .pipe(replace(/("source":\s+\[[^]+"##\s+)\d\d?:\d\d\\n"/, `$1${
+        moment().format('h:mm')
+      }\\n"`))
+      .pipe(rename(path.basename(notebook)))
+      .pipe(gulp.dest(dest));
+  }
 
   return gulp.src(notebookTemplateGlob, {base: templateDir})
     .pipe(newer(notebook))
